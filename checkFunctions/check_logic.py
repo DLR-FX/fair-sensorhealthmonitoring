9ecec1b22d6ff4b72b828271b50d8740d0eb6b90
Klein, Colin <colin.klein@dlr.de>
2023-03-16 02:18:09 +0100

    implementation up to level 2

--- checkFunctions/check_logic.py
+++ checkFunctions/check_logic.py
@@ -1,14 +1,19 @@
+import pandas as pd
+
 from auxiliaryParsing import pickleFunctions
 import checkFunctions.sendMail as sendMail
 from readFunctions import readSensorInformation as Rsi
 from auxiliaryParsing.busDefinitions import labels_inetx
-from Parsing.parseFunctions import get_variables_from_database, find_string_list_in_string_list
+from Parsing.parseFunctions import get_variables_from_database, find_string_list_in_string_list, get_mean_noise, \
+    timestamp_from_utc, utc_from_timestamp
 from stashclient.client import Client
 from readFunctions.readSensorInformation import config_from_istar_flight
+import pandas as pd
+from tqdm import tqdm
 
 
 class check_logic():
-    def __init__(self, data=None, stash_check=False):
+    def __init__(self, data=None, stash_check=False, instance="dev"):
         self.warning = []
         self.layerOneWarning = []
         self.layerTwoWarning = []
@@ -19,7 +24,7 @@ class check_logic():
         self.mailClient = sendMail.MailClient()
         # functionality
         if stash_check:
-            self.check_stash_data(data)
+            self.check_stash_data(data, instance)
         else:
             self.run_functions()
 
@@ -31,39 +36,56 @@ class check_logic():
         self.mailClient.set_mail_content(self.warning)
         # self.mailClient.sendMail()
 
-    def check_stash_data(self, collection: str):
+    def pandas_from_stash(self, id):
+        """
+
+        :param id: stash data series id
+        :type id: str
+        :return: Pandas shape series
+        :rtype: pandas.series
+        """
+        series_connector_id = self.instance.search({"id": id})[0]["series_connector_id"]
+        time_id = self.instance.search({"series_connector_id": series_connector_id, "is_basis_series": True})[0]["id"]
+        time_series = self.instance.data(time_id)
+        data_series = self.instance.data(id)
+        series = pd.Series(index=pd.to_datetime(time_series, unit="s"), data=data_series)
+        return series
+
+    def pandas_to_list(self, series):
+        # datetime to str
+        series.index = [utc_from_timestamp(single_time) for single_time in series.index.asi8 * 1e-9]
+        # return as dictionary
+        return series.to_dict()
+
+    def check_stash_data(self, collection_id, instance="dev"):
         """
         :param collection: collection name
         :return:
         :rtype:
         """
         # get stash Collection name
-        self.instance = Client.from_instance_name("dev")
-        # TODO: get parameter list from stash
-        project_id = self.instance.search({"name": "FX ISTAR", "type": "project"})[0]["id"]
-        flight_list = self.instance.search({"parent": project_id, "name": collection, "type": "collection"})
-        # TODO: get parameter reference list from imcexp
+        self.instance = Client.from_instance_name(instance)
+        # get parameter list from stash
+        flight_list = self.instance.search({"id": collection_id})
+        # get parameter reference list from imcexp
         if flight_list[0]["user_tags"]["registration"] == "D-BDLR":
             config = config_from_istar_flight(flight_list[0]["name"])
         else:
             print("unknown aircraft")
-        # level 1, get parameter names from stash
-        parameter_list = self.instance.search(
-            {"parent": flight_list[0]["id"], "type": "series", "is_basis_series": False})
-        parameters = [parameter.get('user_tags')["Name"]["value"] for parameter in parameter_list if
-                      "user_tags" in parameter and "Name" in parameter.get("user_tags")]
-        # this has to be used to program out martins parameter abbreviations
-        parameters.extend([parameter.get("name") for parameter in parameter_list
-                           if "user_tags" in parameter or "Name" in parameter.get("user_tags")])
-        # use "any" logic if it doesn't match to anything
-        missing_parameters = [param for param in config.keys()
-                              if not any(stash_param in param for stash_param in parameters)]
-        # upload to stash. append to user_tags shm flag of flight
-        flight_list[0]["user_tags"].update({"shm": {"missing_parameters": missing_parameters}})
-        self.instance.update(flight_list[0]["id"], {"user_tags": flight_list[0]["user_tags"]})
+            return
 
-        # parameter real name below usertags->name->value
-        # TODO: level 2
+        missing_parameters, flight_parameters = self.layer_one_from_stash(collection_id, config.keys())
+        print("level 1 check complete")
+        level_2_notes = self.layer_two_from_stash(collection_id, config)
+        print("level 2 check complete")
+
+        # TODO: Level 3
+        # upload to stash. append to user_tags:SHM
+        shm_dict = {"SHM": {"missing parameters": missing_parameters,
+                            "single sensor behaviour": level_2_notes}}
+        flight_list[0]["user_tags"].update(shm_dict)
+        self.instance.update(flight_list[0]["id"], {"user_tags": flight_list[0]["user_tags"]})
+        print("upload complete")
 
     def get_parameter_from_stash(self):
         pass
@@ -85,6 +107,34 @@ class check_logic():
     ╚══════╝╚══════╝░░░╚═╝░░░╚══════╝╚══════╝  ╚══════╝    
     """
 
+    def layer_one_from_stash(self, flight_id, config_parameters):
+        """
+
+        :param flight_id: stash hexadecimal identifier
+        :type flight_id: str
+        :param config_parameters: list of parameters that are in config
+        :type config_parameters: list
+        :return: list of parameters that are in config but not in stash
+        :rtype: list
+        """
+        # level 1, get parameter names from stash
+        parameter_list = self.instance.search(
+            {"parent": flight_id, "type": "series", "is_basis_series": False})
+
+        # this has to be used to program out martins parameter abbreviations
+        parameter_names = []
+        for parameter in parameter_list:
+            if "user_tags" in parameter and "Name" in parameter.get("user_tags"):
+                parameter_names.append(parameter.get('user_tags')["Name"]["value"])
+            else:
+                print(parameter.get("name"))
+                parameter_names.append(parameter.get("name"))
+
+        # use "any" logic if it doesn't match to anything
+        missing_parameters = [param for param in config_parameters
+                              if not any(stash_param in param for stash_param in parameter_names)]
+        return missing_parameters, parameter_names
+
     def layer_one_check(self):
         # check if each parameter in requiredlist is in the listKeys from the dictonary
         self.layerOneWarning = [parameter for parameter in self.requiredParameterList if
@@ -98,6 +148,78 @@ class check_logic():
     ███████╗███████╗░░╚██╔╝░░███████╗███████╗  ███████╗
     ╚══════╝╚══════╝░░░╚═╝░░░╚══════╝╚══════╝  ╚══════╝
     """
+
+    def update_notes(self, notes, check_series, min_range, max_range, warning_string, parameter_name):
+        """
+        cleanup function. Checks whether values are in range and updates the notes section of SHM to display any
+        anomalies
+        :param notes:
+        :type notes:
+        :param check_series:
+        :type check_series:
+        :param min_range:
+        :type min_range:
+        :param max_range:
+        :type max_range:
+        :param warning_string:
+        :type warning_string:
+        :param parameter_name:
+        :type parameter_name:
+        :return:
+        :rtype:
+        """
+        values_exceeded = check_series[~check_series.between(min_range, max_range)]
+        if len(values_exceeded) > 0:
+            notes[warning_string].update({parameter_name: {
+                "occurences": self.pandas_to_list(values_exceeded),
+                "range": {"min": min_range, "max": max_range}}})
+
+    def layer_two_from_stash(self, collection_id, config):
+        # parameter id under usertags->name->value
+        # LEVEL 2
+        parameter_list = self.instance.search(
+            {"parent": collection_id, "type": "series", "is_basis_series": False})
+        # check definition
+        checks = {"physical": {"error_tag": "physical values out of range",
+                               "min": "physical range min",
+                               "max": "physical range max"},
+                  "amplitude": {"error_tag": "amplitude out of range",
+                                "min": "amplitude min",
+                                "max": "amplitude max"}}
+        notes = {value["error_tag"]: {} for value in checks.values()}
+        column_names = []
+        for value in checks.values():
+            column_names.extend([value.get("min"), value.get("max")])
+
+        progress_bar = tqdm(parameter_list)
+        for parameter in progress_bar:
+            parameter_name = parameter["user_tags"]["Name"]["value"]
+            parameter_config = config[parameter_name]
+            progress_bar.set_description("Processing %s" % parameter_name)
+            # check if any of the given checks for min and max are within the parameter config
+            if any(level_2_check in list(parameter_config.keys()) for level_2_check in column_names):
+                # download parameter data into pandas series
+                series = self.pandas_from_stash(parameter["id"])
+                for check in checks.values():
+                    range = [parameter_config.get(check["min"]), parameter_config.get(check["max"])]
+                    if any(range):
+                        self.update_notes(notes, series, range[0], range[1], check["error_tag"], parameter_name)
+                """
+                # check range
+                min_range, max_range = parameter_config.get("physical range min"), \
+                                       parameter_config.get("physical range max")
+                if max_range is not None and min_range is not None:
+                    self.update_notes(notes, series, min_range, max_range,
+                                      "Physical Values out of Range", parameter_name)
+                # check noise and get list with time, where values were exceeded
+                min_amplitude, max_amplitude = parameter_config.get("amplitude min"), \
+                                               parameter_config.get("amplitude max")
+                if min_amplitude is not None and max_amplitude is not None:
+                    mean_noise = get_mean_noise(series)[0]
+                    self.update_notes(notes, mean_noise, min_amplitude, max_amplitude,
+                                      "Amplitude out of range", parameter_name)
+                """
+        return notes
 
     def layer_two_check(self):
         self.create_layer_two_list()
