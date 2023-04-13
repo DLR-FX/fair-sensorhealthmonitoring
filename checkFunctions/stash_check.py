03e991168b07464313fe869ebf138b33dd6d271f
Klein, Colin <colin.klein@dlr.de>
2023-04-13 15:19:46 +0200

    safety commit

--- checkFunctions/stash_check.py
+++ checkFunctions/stash_check.py
@@ -1,5 +1,5 @@
 from checkFunctions.check_logic import check_logic
 
 #
-flight_id = "640f5468e1d767e407e1bdb0"
+flight_id = "64215c47c84cd08f5ab01505"
 der_checker = check_logic(data=flight_id, stash_check=True, instance="dev")
